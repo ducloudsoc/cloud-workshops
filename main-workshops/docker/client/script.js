c86f34ec4bda5b3c6ce0195dba80de847dbb9d77
Will Woodward <will@woodwardweb.com>
2024-10-16 14:37:06 +0100

    fixed nginx config

--- main-workshops/docker/client/script.js
+++ main-workshops/docker/client/script.js
@@ -1,5 +1,6 @@
 document.getElementById('myButton').addEventListener('click', async function() {
     let response = await fetch("/api/volume")
+    response = await response.text()
 
-    document.getElementById('message').innerText = response.text;
+    document.getElementById('message').innerText = response;
 });
