6580ace9bbe3d39e6bffa2ee758bfe2cb848b0cb
Will Woodward <will@woodwardweb.com>
2024-11-04 15:18:56 +0000

    kubernetes example updated

--- 02-kubernetes/example/server/hello_world.py
+++ 02-kubernetes/example/server/hello_world.py
@@ -5,11 +5,3 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello World!</p>"
-
-@app.route("/volume")
-def volume():
-    try:
-        with open("volume.txt", 'r') as file:
-            return file.read()
-    except:
-        return
